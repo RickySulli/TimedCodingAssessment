7ae594010bd60b649f9f6da4072aee58a5cb0cf2
Ricky Sullivan <rickyplaysdrums@gmail.com>
2020-10-19 22:49:49 -0500

    3papiqs

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,14 +23,30 @@ var Timer = setInterval(function(){
 
 let questions = [];
 
-fetch("questions.json")
+fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple")
 .then(res => {
     return res.json();
 })
 .then(loadedQuestions => {
-    console.log(loadedQuestions);
-    questions = loadedQuestions;
-    startGame();
+    console.log(loadedQuestions.results);
+   questions = loadedQuestions.results.map(loadedQuestion =>{
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+    const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random()*3) +1;
+        answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer)
+        
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion ["choice" + (index+1)] = choice;
+        
+        })
+        return formattedQuestion;
+    });
+
+ 
+ startGame();
+
 })
 .catch(err => {
     console.error(err);
