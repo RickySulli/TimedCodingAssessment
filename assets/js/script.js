02c77302cbdd17d2de7b2894ddc3f141951a616c
Ricky Sullivan <rickyplaysdrums@gmail.com>
2020-10-19 23:08:56 -0500

    loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const questionCountertext = document.getElementById('questionCounter');
 const scoretext = document.getElementById('score');
-
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -58,8 +59,9 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    
     getNewQuestion();
+    game.classList.remove('hidden');
+ loader.classList.add('hidden')
 };
 
 getNewQuestion = () => {
